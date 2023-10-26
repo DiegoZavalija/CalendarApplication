3d749c3a629bb277f37201e358778acd015567c6
diegozavalija <diego.zavalija@gmail.com>
2023-10-26 21:53:21 +0800

    CalendarDisplay logic

--- coreapp/src/main/java/Main.java
+++ coreapp/src/main/java/Main.java
@@ -1,17 +1,26 @@
-
-import terminalops.TerminalGrid;
-
+import display.CalendarDisplay;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Main
 {
-    public static void main(String[] args)
-    {
-        var terminalGrid = TerminalGrid.create();
-        String[][] messages = {{"Hello", "Adios"}};
+    public static void main(String[] args) {
+        // Step 1: Create an instance of the CalendarDisplay class with column headings
+        List<String> colHeadings = new ArrayList<>();
+        colHeadings.add("Time");
+        colHeadings.add("Monday");
+        colHeadings.add("Tuesday");
 
-        terminalGrid.print(messages);
+        var calendarDisplay = new CalendarDisplay(colHeadings);
+
+        var row = new ArrayList<String>();
+
+        row.add("1");
+        row.add("2");
+        row.add("3");
+
+        // Step 3: Display the grid
+        calendarDisplay.addRow("Heading", row);
+        calendarDisplay.displayGrid();
     }
 }
---
+++ coreapp/src/main/java/display/CalendarDisplay.java
@@ -0,0 +1,60 @@
+package display;
+import terminalops.TerminalGrid;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalendarDisplay
+{
+    private final TerminalGrid terminalGrid = TerminalGrid.create();
+    private final List<List<String>> listMessages;
+    private final List<String> rowHeadings;
+    private List<String> colHeadings;
+
+    int defaultColLength;
+
+    public CalendarDisplay(List<String> colHeadings)
+    {
+        this.listMessages = new ArrayList<>();
+        this.rowHeadings = new ArrayList<>();
+        setColHeadings(colHeadings);
+
+
+    }
+
+    /**
+     * Adds a heading to the row headings list.
+     * @param rowHeading The row heading to add.
+     */
+    private void addRowHeading(String rowHeading)
+    {
+        rowHeadings.add(rowHeading);
+    }
+
+
+    private void setColHeadings(List<String> colHeadings)
+    {
+        if (colHeadings.isEmpty())
+        {
+            throw new IllegalArgumentException("Give more than one column");
+        }
+        this.colHeadings = colHeadings;
+        this.defaultColLength = colHeadings.size();
+    }
+
+    public void addRow(String rowHeading, List<String> rowContent)
+    {
+
+
+        addRowHeading(rowHeading);
+        listMessages.add(new ArrayList<>(rowContent));
+    }
+
+    public void displayGrid()
+    {
+        terminalGrid.print(this.listMessages, this.rowHeadings, this.colHeadings);
+    }
+
+    // Additional methods can be added here for more functionality.
+    // For example, methods to reset the grid, remove rows/columns, etc.
+}
